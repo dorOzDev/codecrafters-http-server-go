40c6e72267401c1b8832ae8d1cf52dc7591a453e
dorOzDev <dorzry@gmail.com>
2025-05-04 15:14:41 +0300

    add hard coded http response

--- app/main.go
+++ app/main.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -22,9 +18,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
